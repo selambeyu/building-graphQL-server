80d2630b1d35fd6a5f3dcc0d30be08a2857396ef
melkam_beyene <melkam3392beyu@gmail.com>
2022-04-04 15:38:56 +0300

    :zap: :star: add, delete and edit customer using Mutations

--- schema.js
+++ schema.js
@@ -58,6 +58,58 @@ const RootQuery = new GraphQLObjectType({
   },
 });
 
+// Mutuation
+
+const mutuations= new GraphQLObjectType ({
+    name:"Mutation",
+    fields:{
+        addCustomer:{
+            type:CustomerType,
+            args:{
+                name:{type:new GraphQLNonNull(GraphQLString) },
+                email:{type:new GraphQLNonNull(GraphQLString) },
+                age:{type:new GraphQLNonNull(GraphQLInt) }
+            },
+            resolve(parentValue,args){
+                return axios.post("http://localhost:3000/customers/",{
+                    name:args.name,
+                    email:args.email,
+                    age:args.age
+                }).then(res=>res.data)
+            }
+        },
+        deleteCustomer:{
+            type:CustomerType,
+            args:{
+                id:{type:new GraphQLNonNull(GraphQLString) },
+                
+            },
+            resolve(parentValue,args){
+                return axios.delete("http://localhost:3000/customers/"+ args.id).then(res=>res.data)
+            }
+        },
+        editCustomer:{
+            type:CustomerType,
+            args:{
+                id:{type:new GraphQLNonNull(GraphQLString) },
+                name:{type:new GraphQLNonNull(GraphQLString) },
+                email:{type:new GraphQLNonNull(GraphQLString) },
+                age:{type:new GraphQLNonNull(GraphQLInt) }
+            },
+            resolve(parentValue,args){
+                return axios.patch("http://localhost:3000/customers/"+ args.id,
+                {
+                    name:args.name,
+                    email:args.email,
+                    age:args.age
+                }).then(res=>res.data)
+            }
+        },
+        
+    }
+})
+
 module.exports = new GraphQLSchema({
   query: RootQuery,
+  mutation:mutuations
 });
